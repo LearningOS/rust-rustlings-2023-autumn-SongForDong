0aca47703af6aa1d68363d714631fb2100215901
SongForDong <844436821@qq.com>
2023-10-16 16:05:27 +0800

    update

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -17,7 +17,7 @@
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
 // I AM NOT DONE
-
+//use std::fmt::Display;
 pub struct ReportCard {
     pub grade: f32,
     pub student_name: String,
